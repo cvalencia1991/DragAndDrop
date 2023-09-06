ef4aed6c9047959273a5bc583a5611c10e82eb15
Cesar Valencia <cesar4a6z@gmail.com>
2023-09-06 16:43:11 -0500

    Optimize Validator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,34 @@
+// Validation
+
+interface Validatable {
+  value: string | number;
+  required: boolean,
+  minLength?: number,
+  maxLength?: number,
+  min?: number,
+  max?: number
+}
+
+function validate (validatableInput:Validatable ){
+  let isValid = true;
+  if(validatableInput.required){
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if(validatableInput.minLength != null && typeof validatableInput.value === 'string'){
+    isValid = isValid && validatableInput.value.length >=  validatableInput.minLength
+  }
+  if(validatableInput.maxLength != null && typeof validatableInput.value === 'string'){
+    isValid = isValid && validatableInput.value.length >  validatableInput.maxLength
+  }
+  if (validatableInput.min != null && typeof validatableInput.value === 'number'){
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (validatableInput.max != null && typeof validatableInput.value === 'number'){
+    isValid = isValid && validatableInput.value > validatableInput.max;
+  }
+  return isValid
+}
+
 // AutoBind Decorator
 
 function Autobind(_:any, _2:string, descriptor: PropertyDescriptor){
@@ -45,9 +76,23 @@ class ProjectInput {
     const enteredDescription  = this.descriptionInputElement.value
     const enterededPeople = this.peopleInputElement.value
 
-    if(enteredTitle.trim().length === 0 ||
-    enteredDescription.trim().length === 0 ||
-    enterededPeople.trim().length === 0){
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required:true
+    }
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required:true,
+      minLength: 5
+    }
+    const peopleValidatable: Validatable = {
+      value: +enterededPeople,
+      required:true,
+      min: 1,
+      max: 10
+    }
+
+    if(!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)){
       alert('invalid Inpuyt, please try again!')
       return;
     }else{
