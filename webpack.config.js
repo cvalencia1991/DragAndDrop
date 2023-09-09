9fcb81b975dbd27a599c6e142feccecaecaeb188
Cesar Valencia <cesar4a6z@gmail.com>
2023-09-09 18:34:02 -0500

    Formatting Document

--- webpack.config.js
+++ webpack.config.js
@@ -4,28 +4,24 @@ module.exports = {
     mode: 'development',
     entry: './src/app.ts',
     devtool: 'inline-source-map',
-    devServer:{
-        static: [
-            {
-                directory: path.join(__dirname)
-            }
-        ]
+    devServer: {
+        static: [{
+            directory: path.join(__dirname)
+        }]
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname,'dist'),
+        path: path.resolve(__dirname, 'dist'),
         publicPath: '/dist/'
     },
     module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-        ]
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }, ]
     },
     resolve: {
-        extensions:['.ts' , '.js']
+        extensions: ['.ts', '.js']
     }
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,27 +6,23 @@ module.exports = {
     entry: './src/app.ts',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname,'dist'),
+        path: path.resolve(__dirname, 'dist'),
     },
     devtool: 'inline-source-map',
-    devServer:{
-        static: [
-            {
-                directory: path.join(__dirname)
-            }
-        ]
+    devServer: {
+        static: [{
+            directory: path.join(__dirname)
+        }]
     },
     module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-        ]
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }, ]
     },
     resolve: {
-        extensions:['.ts' , '.js']
+        extensions: ['.ts', '.js']
     },
     plugins: [
         new CleanPlugin.CleanWebpackPlugin()
